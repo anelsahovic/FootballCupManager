a3a11786609d85e50f91d098b7880cb9ff6716db
Anel <anel.sahovic.bsc@gmail.com>
2024-08-06 18:57:55 +0200

    Almost done creating Fxml files

--- src/main/java/footballcupmanager/Main.java
+++ src/main/java/footballcupmanager/Main.java
@@ -1,9 +1,15 @@
 package footballcupmanager;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +18,15 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/FxmlFiles/HomeScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(getClass().getResource("/CssFiles/main.css").toExternalForm());
+        AnchorPane root = fxmlLoader.load();
+
+        // Load the background image
+        Image backgroundImage = new Image(Main.class.getResource("/images/background.jpg").toExternalForm());
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        root.setBackground(new Background(background));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Main.class.getResource("/CssFiles/main.css").toExternalForm());
         stage.setTitle("Football Cup Manager!");
         stage.setScene(scene);
         stage.show();
